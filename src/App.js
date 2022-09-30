336ac5ea3097e78bab512af01deea1375875d3e5
Devon McKenzie <novithian@gmail.com>
2022-09-30 16:49:12 -0400

    nomore cra

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<>
+			<h1>React Application Template</h1>
+			<span>Pretty empty..</span>
+		</>
+	);
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "public"),
+		filename: "main.js",
+	},
+	target: "web",
+	devServer: {
+		port: "3000",
+		static: ["./public"],
+		open: true,
+		hot: true,
+		liveReload: true,
+		client: {
+			overlay: false,
+			logging: "warn",
+		},
+	},
+	resolve: {
+		extensions: [".js", ".jsx", ".json", ".ts"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
